f2a62b5edc2a651db4a54e5d8d7b73a7bee7da53
Adam Damiano <onaimada@gmail.com>
2017-10-20 14:35:45 -0700

    Fix arrow position on tooltip for 'top'
    Previously, the arrow would not necessarily point at the element. There
    is a concept of "bottom-middle-aligned", "bottom-right-aligned", etc.,
    but for some reason it doesn't apply to "top".

--- intro.js
+++ intro.js
@@ -517,6 +517,8 @@
     tooltipOffset = _getOffset(tooltipLayer);
     windowSize    = _getWinSize();
 
+    let alignArrowHorizontally = false;
+
     switch (currentTooltipPosition) {
       case 'top':
         arrowLayer.className = 'introjs-arrow bottom';
@@ -529,6 +531,7 @@
 
         _checkRight(targetOffset, tooltipLayerStyleLeft, tooltipOffset, windowSize, tooltipLayer);
         tooltipLayer.style.bottom = (targetOffset.height +  20) + 'px';
+        alignArrowHorizontally = true;
         break;
       case 'right':
         tooltipLayer.style.left = (targetOffset.width + 20) + 'px';
@@ -607,7 +610,15 @@
         var tooltipLayerStyleLeft = 0;
         _checkRight(targetOffset, tooltipLayerStyleLeft, tooltipOffset, windowSize, tooltipLayer);
         tooltipLayer.style.top    = (targetOffset.height +  20) + 'px';
+        alignArrowHorizontally = true;
         break;
+    }
+
+    if (alignArrowHorizontally) {
+        const tooltipRect = tooltipLayer.getBoundingClientRect();
+        const targetRect = targetElement.getBoundingClientRect();
+        const arrowRect = arrowLayer.getBoundingClientRect();
+        arrowLayer.style.left = Math.round(targetRect.x - tooltipRect.x + targetRect.width / 2 - arrowRect.width / 2) + 'px';
     }
   }
 
